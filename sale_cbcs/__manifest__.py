b44d00b466acb36da1b18b060f752b8637469829
m-samiullah <muhammadsamiullahqadeer@gmail.com>
2025-06-14 21:45:37 +0500

    Ignore Some un nessaccry file

---
+++ sale_cbcs/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Sales Management (CBCS)',
+    'author': 'Cariboucs Consultancy Services',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        # 'views/management.xml',
+        # 'wizards/client_register_wizard.xml',
+        # 'data/data.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    'maintainer': 'Cariboucs Consultancy Services',
+    'auto_install': False,
+    'sequence': 0,
+    'category': 'Sales',
+    'summary': 'Management',
+    'description': """
+                                            Sales Management Module
+                                            =======================
+        This module provides a complete solution to manage the sales workflow in your organization. It allows users to handle customer quotations, sales orders, and invoicing 
+        with full integration to products, pricing, and customer data.
+    """,
+    'website': 'https://www.cariboucs.com',
+}
